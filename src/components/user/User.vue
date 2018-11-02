e3e087caa0e1d428f18dd529a1e1f7e16c4122a7
Daley <daley.mottley@hotmail.com>
2018-11-02 06:57:03 +0000

    Add user id parameter

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-    <h1>The User Page</h1>
-    <hr>
-    <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+        <h1>The User Page</h1>
+        <hr>
+        <p>Loaded ID: {{ id }} </p>
+        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
         methods: {
             navigateToHome() {
                 this.$router.push('/');
